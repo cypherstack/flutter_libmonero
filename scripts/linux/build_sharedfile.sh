72362d1b618ac36e8c79a8ddab8c67c23237cb06
julian-CStack <julian@cypherstack.com>
2023-03-21 17:15:42 -0600

    initial purge

--- scripts/linux/build_sharedfile.sh
+++ scripts/linux/build_sharedfile.sh
@@ -12,17 +12,10 @@ OS="LINUX"
 sed -i "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$COMMIT\";" $VERSIONS_FILE
 cd build
 mkdir monero_build
-mkdir wownero_build
 MONERO_BUILD=$(pwd)/monero_build
-WOWNERO_BUILD=$(pwd)/wownero_build
 
 cd $MONERO_BUILD
 cmake ../../cmakefiles/monero/${TYPES_OF_BUILD}
 make -j$(nproc)
 cp libcw_monero.so ../
-
-cd $WOWNERO_BUILD
-cmake ../../cmakefiles/wownero/${TYPES_OF_BUILD}
-make -j$(nproc)
-cp libcw_wownero.so ../
 

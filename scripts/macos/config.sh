a09e2ab8121a58643fb43698eb65d55d69534111
julian-CStack <julian@cypherstack.com>
2023-07-14 13:35:30 -0600

    .

--- scripts/macos/config.sh
+++ scripts/macos/config.sh
@@ -2,7 +2,7 @@
 
 export MACOS_SCRIPTS_DIR=`pwd`
 export CW_ROOT=${MACOS_SCRIPTS_DIR}/../..
-export EXTERNAL_DIR=${CW_ROOT}/cw_shared_external/macos/External
+export EXTERNAL_DIR=${CW_ROOT}/cw_shared_external/ios/External
 export EXTERNAL_MACOS_DIR=${EXTERNAL_DIR}/macos
 export EXTERNAL_MACOS_SOURCE_DIR=${EXTERNAL_MACOS_DIR}/sources
 export EXTERNAL_MACOS_LIB_DIR=${EXTERNAL_MACOS_DIR}/lib

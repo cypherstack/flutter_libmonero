27442350f6f738a5431662c48017855db699e2ba
sneurlax <sneurlax@gmail.com>
2024-01-31 17:57:47 -0600

    add path to app dir for windows

--- cw_core/lib/pathForWallet.dart
+++ cw_core/lib/pathForWallet.dart
@@ -41,8 +41,7 @@ Future<Directory> applicationRootDirectory() async {
   } else if (Platform.isLinux) {
     appDirectory = Directory("${Platform.environment['HOME']}/.stackwallet");
   } else if (Platform.isWindows) {
-// TODO: windows root .stackwallet dir location
-    throw Exception("Unsupported platform");
+    appDirectory = Directory("${Platform.environment['APPDATA']}/stackwallet");
   } else if (Platform.isMacOS) {
     appDirectory = await getLibraryDirectory();
     appDirectory = Directory("${appDirectory.path}/stackwallet");

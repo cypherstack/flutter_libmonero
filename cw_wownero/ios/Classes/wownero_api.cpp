5e8092222f75e428dc968c0a8edb28d73b1e009c
sneurlax <sneurlax@gmail.com>
2024-01-31 14:48:29 -0600

    expand DLL_EXPORT macro for BUILD_FOR_WINDOWS

--- cw_wownero/ios/Classes/wownero_api.cpp
+++ cw_wownero/ios/Classes/wownero_api.cpp
@@ -27,7 +27,7 @@ void __clear_cache(void* start, void* end) { }
 void nice(int niceness); // Prototype definition
 
 // Define a macro for exporting functions on Windows.
-#if defined(_WIN32) || defined(_WIN64) || defined(__declspec)
+#if defined(_WIN32) || defined(_WIN64) || defined(__declspec) || defined(BUILDING_FOR_WINDOWS)
 #define NICE(pri) 
 #define DLL_EXPORT __declspec(dllexport)
 #else

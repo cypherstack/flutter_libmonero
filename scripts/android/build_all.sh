18f41439b19a80ac9d0bd7611054d96e83077e06
julian <julian@cypherstack.com>
2024-06-25 16:03:10 -0600

    remaining triples

--- scripts/android/build_all.sh
+++ scripts/android/build_all.sh
@@ -35,7 +35,7 @@ then
         pushd ../monero_c
             docker run --platform linux/amd64 -v$HOME/.cache/ccache:/root/.ccache -v$PWD:$PWD -w $PWD --rm -it git.mrcyjanek.net/mrcyjanek/debian:buster bash -c "git config --global --add safe.directory '*'; apt update; apt install -y ccache gcc g++ libtinfo5 gperf; ./build_single.sh ${COIN} x86_64-linux-android $NPROC"
             # docker run --platform linux/amd64 -v$PWD:$PWD -w $PWD --rm -it git.mrcyjanek.net/mrcyjanek/debian:buster bash -c "git config --global --add safe.directory '*'; apt update; apt install -y ccache gcc g++ libtinfo5 gperf; ./build_single.sh ${COIN} i686-linux-android $NPROC"
-            docker run --platform linux/amd64 -v$HOME/.cache/ccache:/root/.ccache -v$PWD:$PWD -w $PWD --rm -it git.mrcyjanek.net/mrcyjanek/debian:buster bash -c "git config --global --add safe.directory '*'; apt update; apt install -y ccache gcc g++ libtinfo5 gperf; ./build_single.sh ${COIN} arm-linux-androideabi $NPROC"
+            docker run --platform linux/amd64 -v$HOME/.cache/ccache:/root/.ccache -v$PWD:$PWD -w $PWD --rm -it git.mrcyjanek.net/mrcyjanek/debian:buster bash -c "git config --global --add safe.directory '*'; apt update; apt install -y ccache gcc g++ libtinfo5 gperf; ./build_single.sh ${COIN} armv7a-linux-androideabi $NPROC"
             docker run --platform linux/amd64 -v$HOME/.cache/ccache:/root/.ccache -v$PWD:$PWD -w $PWD --rm -it git.mrcyjanek.net/mrcyjanek/debian:buster bash -c "git config --global --add safe.directory '*'; apt update; apt install -y ccache gcc g++ libtinfo5 gperf; ./build_single.sh ${COIN} aarch64-linux-android $NPROC"
         popd
     done
@@ -45,7 +45,7 @@ else
         pushd ../monero_c
             ./build_single.sh ${COIN} x86_64-linux-android $NPROC
             # ./build_single.sh ${COIN} i686-linux-android $NPROC
-            ./build_single.sh ${COIN} arm-linux-androideabi $NPROC
+            ./build_single.sh ${COIN} armv7a-linux-androideabi $NPROC
             ./build_single.sh ${COIN} aarch64-linux-android $NPROC
         popd
     done
@@ -53,14 +53,14 @@ fi
 
 unxz -f ../monero_c/release/monero/x86_64-linux-android_libwallet2_api_c.so.xz
 unxz -f ../monero_c/release/wownero/x86_64-linux-android_libwallet2_api_c.so.xz
-unxz -f ../monero_c/release/monero/arm-linux-androideabi_libwallet2_api_c.so.xz
-unxz -f ../monero_c/release/wownero/arm-linux-androideabi_libwallet2_api_c.so.xz
+unxz -f ../monero_c/release/monero/armv7a-linux-androideabi_libwallet2_api_c.so.xz
+unxz -f ../monero_c/release/wownero/armv7a-linux-androideabi_libwallet2_api_c.so.xz
 unxz -f ../monero_c/release/monero/aarch64-linux-android_libwallet2_api_c.so.xz
 unxz -f ../monero_c/release/wownero/aarch64-linux-android_libwallet2_api_c.so.xz
 
 ln -s $(realpath ../monero_c/release/monero/aarch64-linux-android_libwallet2_api_c.so) ../../../../android/app/src/main/jniLibs/arm64-v8a/libmonero_libwallet2_api_c.so || true
 ln -s $(realpath ../monero_c/release/wownero/aarch64-linux-android_libwallet2_api_c.so) ../../../../android/app/src/main/jniLibs/arm64-v8a/libwownero_libwallet2_api_c.so || true
-ln -s $(realpath ../monero_c/release/monero/arm-linux-androideabi_libwallet2_api_c.so) ../../../../android/app/src/main/jniLibs/armeabi-v7a/libmonero_libwallet2_api_c.so || true
-ln -s $(realpath ../monero_c/release/wownero/arm-linux-androideabi_libwallet2_api_c.so) ../../../../android/app/src/main/jniLibs/armeabi-v7a/libwownero_libwallet2_api_c.so || true
+ln -s $(realpath ../monero_c/release/monero/armv7a-linux-androideabi_libwallet2_api_c.so) ../../../../android/app/src/main/jniLibs/armeabi-v7a/libmonero_libwallet2_api_c.so || true
+ln -s $(realpath ../monero_c/release/wownero/armv7a-linux-androideabi_libwallet2_api_c.so) ../../../../android/app/src/main/jniLibs/armeabi-v7a/libwownero_libwallet2_api_c.so || true
 ln -s $(realpath ../monero_c/release/monero/x86_64-linux-android_libwallet2_api_c.so) ../../../../android/app/src/main/jniLibs/x86_64/libmonero_libwallet2_api_c.so || true
 ln -s $(realpath ../monero_c/release/wownero/x86_64-linux-android_libwallet2_api_c.so) ../../../../android/app/src/main/jniLibs/x86_64/libwownero_libwallet2_api_c.so || true
